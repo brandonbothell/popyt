de789b5eca1dd96a1cc6d5fc1ee8ceb3767a096a
Jason Bothel <shadowmanguyyt@gmail.com>
2019-10-20 16:42:12 -0400

    Convert gulpfile to typescript

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,15 +1,16 @@
-const gulp = require('gulp')
-const fsn = require('fs-nextra')
-const ts = require('gulp-typescript')
-const sourcemaps = require('gulp-sourcemaps')
-const typedoc = require('gulp-typedoc')
-const merge = require('merge2')
+import * as gulp from 'gulp'
+import * as fsn from 'fs-nextra'
+import * as ts from 'gulp-typescript'
+import * as sourcemaps from 'gulp-sourcemaps'
+import typedoc from 'gulp-typedoc'
+import merge from 'merge2'
+
 const project = ts.createProject('tsconfig.json')
 
 async function build () {
   await Promise.all([
     fsn.emptydir('out'),
-    fsn.emptydir('typings'),
+    fsn.emptydir('typings')
   ])
 
   const result = project.src()
@@ -18,7 +19,7 @@ async function build () {
 
   return merge([
     result.dts.pipe(gulp.dest('typings')),
-    result.js.pipe(sourcemaps.write('.', { sourceRoot: '../src' })).pipe(gulp.dest('out')),
+    result.js.pipe(sourcemaps.write('.', { sourceRoot: '../src' })).pipe(gulp.dest('out'))
   ])
 }
 
