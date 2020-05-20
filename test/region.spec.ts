7fcbe07766299158fe5ad9d63ca8b4cdd22f5661
Jason Bothel <shadowmanguyyt@gmail.com>
2020-05-19 20:53:51 -0400

    test: add region specification

---
+++ test/region.spec.ts
@@ -0,0 +1,33 @@
+import 'mocha'
+import { expect } from 'chai'
+import { youtube } from './cache.spec'
+import { Region } from '../src'
+
+const apiKey = process.env.YOUTUBE_API_KEY
+
+if (!apiKey) {
+  throw new Error('No API key')
+}
+
+describe('Regions', () => {
+  it('should set all available properties', async () => {
+    let region = (await youtube.getRegions())[0]
+
+    expect(region.full).to.equal(true)
+    expect(region.id).to.be.a('string')
+    expect(region.gl).to.be.a('string')
+    expect(region.name).to.be.a('string')
+  })
+
+  it('should throw an error on invalid type', () => {
+    let error: string = null
+
+    try {
+      new Region(youtube, { kind: 'invalid' })
+    } catch (err) {
+      error = err.message
+    }
+
+    expect(error).to.equal('Invalid region type: invalid')
+  })
+})
