dd09ec64e44f0292e20dd809f3bd200816d7ce56
Jason Bothel <shadowmanguyyt@gmail.com>
2020-05-27 16:12:34 -0400

    test: add channel section channel/playlist fetching tests

--- test/channel-section.spec.ts
+++ test/channel-section.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha'
 import { expect } from 'chai'
 import { youtube } from './cache.spec'
-import { Channel, ChannelSection, Playlist } from '../src'
+import { ChannelSection, Channel, Playlist } from '../src'
 
 const apiKey = process.env.YOUTUBE_API_KEY
 
@@ -13,28 +13,38 @@ let sections: ChannelSection[]
 
 describe('Channel sections', () => {
   it('should set all available properties', async () => {
-    sections = await youtube.getChannelSections('UC6mi9rp7vRYninucP61qOjg')
+    sections = await Promise.all((await youtube.getChannelSections('UC6mi9rp7vRYninucP61qOjg')).map(s => s.fetch()))
 
-    sections.map((s, i) => {
-      return s.fetch()
+    sections.forEach(section => {
+      expect(section.full).to.equal(false)
+
+      expect(section.id).to.be.a('string')
+      expect(section.channelId).to.be.a('string')
+      expect(section.position).to.be.a('number').and.gte(0)
+      expect(section.style).to.be.a('string')
+      expect(section.type).to.be.a('string')
+
+      if (section.name) expect(section.name).to.be.a('string')
+      if (section.localizedName) expect(section.localizedName).to.be.a('string')
+      if (section.language) expect(section.language).to.be.a('string')
+      if (section.localizations) expect(section.localizations).to.be.an('object')
+      if (section.channelIds) expect(section.channelIds).to.be.an('array')
+      if (section.playlistIds) expect(section.playlistIds).to.be.an('array')
     })
+  })
 
-    const section = sections[0]
+  it('should fetch channels', async () => {
+    const section = sections.find(s => s.channelIds)
+    await section.fetchChannels()
 
-    expect(section.full).to.equal(false)
+    expect(section.channels[0]).to.be.an.instanceOf(Channel)
+  })
 
-    expect(section.id).to.be.a('string')
-    expect(section.channelId).to.be.a('string')
-    expect(section.position).to.be.a('number').and.gte(0)
-    expect(section.style).to.be.a('string')
-    expect(section.type).to.be.a('string')
+  it('should fetch playlists', async () => {
+    const section = sections.find(s => s.playlistIds)
+    await section.fetchPlaylists()
 
-    if (section.name) expect(section.name).to.be.a('string')
-    if (section.localizedName) expect(section.localizedName).to.be.a('string')
-    if (section.language) expect(section.language).to.be.a('string')
-    if (section.localizations) expect(section.localizations).to.be.an('object')
-    if (section.channelIds) expect(section.channelIds).to.be.an('array')
-    if (section.playlistIds) expect(section.playlistIds).to.be.an('array')
+    expect(section.playlists[0]).to.be.an.instanceOf(Playlist)
   })
 
   it('should throw an error on invalid type', () => {
