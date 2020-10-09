50b3c43ecf96d9ea449524c97504339910aa0b92
Jason Bothel <shadowmanguyyt@gmail.com>
2020-10-09 14:34:48 -0400

    style: parameter order and test consistency

--- src/services/search-service.ts
+++ src/services/search-service.ts
@@ -6,8 +6,9 @@ import { Cache } from '../util'
  */
 export class SearchService {
   /* istanbul ignore next */
-  public static async search (youtube: YouTube, types: (typeof Video | typeof Channel | typeof Playlist)[], searchTerm: string, maxResults: number = 10, pageToken?: string, channelId?: string,
-    fields?: string, category?: string, onlyEmbeddable: boolean = false, eventType?: 'completed' | 'live' | 'upcoming', videoType?: 'any' | 'episode' | 'movie'):
+  public static async search (youtube: YouTube, types: (typeof Video | typeof Channel | typeof Playlist)[], searchTerm: string, maxResults: number = 10, pageToken?: string,
+    channelId?: string, fields?: string, category?: string, onlyEmbeddable: boolean = false, eventType?: 'completed' | 'live' | 'upcoming',
+    videoType?: 'any' | 'episode' | 'movie'):
   Promise<{ results: (Video | Channel | Playlist)[]; prevPageToken: string; nextPageToken: string }> {
     const type = types.map(t => t.endpoint.substring(0, t.endpoint.length - 1)).join(',')
     const cached = Cache.get(`search://${type}/"${searchTerm}"/${maxResults}/"${pageToken}"`)
